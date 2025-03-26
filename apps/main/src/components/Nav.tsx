89090c24dc8b418bedada7d5be573a3596a4e697
Sarah Gerrard <98355961+LadyBluenotes@users.noreply.github.com>
2025-03-26 16:37:52 -0700

    add workspaces

---
+++ apps/main/src/components/Nav.tsx
@@ -0,0 +1,52 @@
+import { A, useLocation } from "@solidjs/router";
+import { For } from "solid-js";
+
+export const Nav = () => {
+	const location = useLocation();
+
+	const links = [
+		{
+			href: "/about",
+			text: "About",
+		},
+		{
+			href: "/blog",
+			text: "Blog",
+		},
+		{
+			href: "/resume",
+			text: "Resume",
+		},
+		{
+			href: "/oss",
+			text: "Open Source",
+		},
+		{
+			href: "/contact",
+			text: "Contact",
+		},
+	];
+
+	return (
+		<nav class="rounded-lg rounded-b-none border-b-4 border-solid border-black bg-white">
+			<ul class="container flex items-center font-silkscreen">
+				<li class="flex p-4">
+					<div class="w-5 h-5 bg-accent-red rounded-full" />
+				</li>
+				<For each={links}>
+					{({ href, text }) => (
+						<li class="flex">
+							<A
+								href={href}
+								class="ui-button-default"
+								activeClass="underline decoration-4"
+							>
+								{text}
+							</A>
+						</li>
+					)}
+				</For>
+			</ul>
+		</nav>
+	);
+};
