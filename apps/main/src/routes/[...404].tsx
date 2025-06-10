6c9f4afcbf53676247ba13bffa1ff9dc65ef8a69
LadyBluenotes <gerrardsarah@gmail.com>
2025-06-10 13:12:24 -0700

    update themes and 404 page

--- apps/main/src/routes/[...404].tsx
+++ apps/main/src/routes/[...404].tsx
@@ -1,31 +1,51 @@
 import { A } from "@solidjs/router";
+import { createEffect } from "solid-js";
+import Button from "~/components/common/Button";
 
 export default function NotFound() {
-  return (
-    <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
-        Not Found
-      </h1>
-      <p class="mt-8">
-        Visit{" "}
-        <a
-          href="https://solidjs.com"
-          target="_blank"
-          class="text-sky-600 hover:underline"
-        >
-          solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
-      </p>
-      <p class="my-4">
-        <A href="/" class="text-sky-600 hover:underline">
-          Home
-        </A>
-        {" - "}
-        <A href="/about" class="text-sky-600 hover:underline">
-          About Page
-        </A>
-      </p>
-    </main>
-  );
+	createEffect(() => {
+		// add a script tag to the bottom of the page
+		const script = document.createElement("script");
+		script.src =
+			"https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.9.1/highlight.min.js";
+		script.onload = () => {
+			const codeBlock = document.querySelector(".content-codeblock pre code");
+			if (codeBlock) {
+				// @ts-ignore
+				hljs.highlightBlock(codeBlock);
+			}
+		};
+		document.body.appendChild(script);
+	});
+
+	return (
+		<main>
+			<div class="code-area">
+				<div class="section-codeblock">
+					<div class="content-codeblock">
+						<pre class="js">
+							<code>
+								{`// 404 page not found.
+
+  if (!found) {
+    throw ("(╯°□°)╯︵ ┻━┻");
+  }
+`}
+								//
+								<Button
+									as={A}
+									href="/"
+									variant="text"
+									size="small"
+									class="button-404"
+								>
+									click here to return Home
+								</Button>
+							</code>
+						</pre>
+					</div>
+				</div>
+			</div>
+		</main>
+	);
 }
--- packages/styling/index.ts
+++ packages/styling/index.ts
@@ -1,4 +1,3 @@
 import StyleSheets from "./index.css";
-import UnoConfig from "./styles/uno";
 
-export { StyleSheets, UnoConfig };
+export { StyleSheets };
