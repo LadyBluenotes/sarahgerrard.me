5be0443722cf16561843d0e3d08e57a6e1c82456
LadyBluenotes <gerrardsarah@gmail.com>
2025-11-04 21:45:34 -0800

    sort posts by date and year

--- src/routes/writing/index.tsx
+++ src/routes/writing/index.tsx
@@ -7,28 +7,61 @@ export const Route = createFileRoute("/writing/")({
 });
 
 function RouteComponent() {
+	// sort posts by date descending
+
+	// categorize by year
+	allPosts.sort((a, b) => {
+		const dateA = new Date(a.date as string).getTime();
+		const dateB = new Date(b.date as string).getTime();
+		return dateB - dateA;
+	});
+
+	const postsByYear = allPosts.reduce(
+		(acc, post) => {
+			const year = new Date(post.date as string).getFullYear();
+			if (!acc[year]) {
+				acc[year] = [];
+			}
+			acc[year].push(post);
+			return acc;
+		},
+		{} as Record<number, typeof allPosts>
+	);
+
 	return (
 		<div>
 			<ul class="flex flex-col gap-4">
-				<For each={allPosts}>
-					{(post) => (
+				<For each={Object.entries(postsByYear).reverse()}>
+					{([year, posts]) => (
 						<li>
-							<a
-								href={`/writing/${post._meta.path}`}
-								class="group flex gap-2 content-bottom group-hover:underline"
-							>
-								<span class="text-[--inactive] group-hover:text-[--hover-inactive] group-hover:underline">
-									{post.title}
-								</span>
-								-
-								<span class="text-sm mt-auto text-[--inactive]/70">
-									{new Date(post.date as string).toLocaleDateString("en-CA", {
-										year: "numeric",
-										month: "short",
-										day: "numeric",
-									})}
-								</span>
-							</a>
+							<h2 class="text-2xl font-bold mb-2">{year}</h2>
+							<ul class="flex flex-col gap-2">
+								<For each={posts}>
+									{(post) => (
+										<li>
+											<a
+												href={`/writing/${post._meta.path}`}
+												class="group flex gap-2 content-bottom group-hover:underline"
+											>
+												<span class="text-[--inactive] group-hover:text-[--hover-inactive] group-hover:underline">
+													{post.title}
+												</span>
+												-
+												<span class="text-sm mt-auto text-[--inactive]/70">
+													{new Date(post.date as string).toLocaleDateString(
+														"en-CA",
+														{
+															year: "numeric",
+															month: "short",
+															day: "numeric",
+														}
+													)}
+												</span>
+											</a>
+										</li>
+									)}
+								</For>
+							</ul>
 						</li>
 					)}
 				</For>
