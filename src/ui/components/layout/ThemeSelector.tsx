46beceab032551bf733a6be1c526270212171d53
LadyBluenotes <gerrardsarah@gmail.com>
2025-11-19 08:20:33 -0800

    update theme selector page

--- src/ui/components/layout/ThemeSelector.tsx
+++ src/ui/components/layout/ThemeSelector.tsx
@@ -4,22 +4,30 @@ import { Button } from "@kobalte/core/button";
 export const ThemeSelector = () => {
 	const [darkMode, setDarkMode] = createSignal<boolean>(false);
 
+	onMount(() => {
+		const theme = window.localStorage.getItem("theme");
+		if (theme === "dark") {
+			setDarkMode(true);
+			document.documentElement.classList.add("dark");
+		}
+	})
+
 	createEffect(
-		on(darkMode, () => {
-			if (!darkMode()) {
-				document.documentElement.setAttribute("data-theme", "dark");
-				window.localStorage.setItem("theme", "dark");
-			} else {
-				document.documentElement.setAttribute("data-theme", "light");
-				window.localStorage.setItem("theme", "light");
-			}
+		on(darkMode, (isDark) => {
+		if(isDark) {
+			document.documentElement.setAttribute("data-theme", "dark");
+		} else {
+			document.documentElement.removeAttribute("data-theme");
+		}
+			window.localStorage.setItem("theme", isDark ? "dark" : "light");
 		})
 	);
 
+	// noinspection TypeScriptValidateTypes
 	return (
 		<Button
 			aria-label="Toggle Theme"
-			onClick={() => setDarkMode(!darkMode())}
+			onClick={() => setDarkMode(prev => !prev)}
 			class="color-[--inactive] hover:color-[--hover-inactive]"
 		>
 			<Show
